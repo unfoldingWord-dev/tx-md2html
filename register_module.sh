0ff95b40447c8ad7e3554ab3ba664cb9977f633b
Richard Mahn <richmahn@users.noreply.github.com>
2016-09-23 07:04:53 -0600

    Update register_module.sh

--- register_module.sh
+++ register_module.sh
@@ -2,5 +2,5 @@
 
 API_URL=$1
 
-curl --header "Content-Type: application/json" -X POST --data @functions/register/module.json $API_URL/tx/module
+curl --header "Content-Type: application/json" -X POST --data @functions/register/module.json $API_URL
 
