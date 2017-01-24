b068c292a510434a7142fc162ad37d7d739c5d04
Bruce McLean <mcleanb@photo-nomad.net>
2017-01-24 15:17:47 -0500

    Added TransformOBS unit test from internal test file.

--- tests/test_transformOBS_internal.py
+++ tests/test_transformOBS_internal.py
@@ -1,128 +1,143 @@
-# test Transform OBS from md to html using internal files
+# test Transform OBS from md to html using external url
 
 from __future__ import print_function, unicode_literals
-
-import codecs
 import os
 import shutil
 import tempfile
-import markdown
-import string
+from contextlib import closing
+import unittest
+from functions.convert.transform_obs import TransformOBS
 
-from glob import glob
-from shutil import copyfile
-from general_tools.file_utils import unzip, make_dir, write_file
+from general_tools.file_utils import unzip, get_subdirs, write_file, add_contents_to_zip, add_file_to_zip
 from general_tools.url_utils import download_file
-from door43_tools.obs_handler import OBSInspection
+from door43_tools.preprocessors import TsObsMarkdownPreprocessor
+from door43_tools.manifest_handler import Manifest, MetaData
 
 
-class TransformOBS(object):
+class TestTransformOBS(unittest.TestCase):
 
-    def __init__(self, source_url, output_dir, options):
-        self.source_url = source_url
-        self.output_dir = output_dir
-        self.options = options
-        self.download_dir = tempfile.mkdtemp(prefix='download_')
-        self.files_dir = tempfile.mkdtemp(prefix='files_')
-        self.log = []
-        self.warnings = []
-        self.errors = []
-        self.failed = False;
+    resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
 
-    def log_message(self, message):
-        print(message)
-        self.log.append(message)
+    def setUp(self):
+        """
+        Runs before each test
+        """
+        self.out_dir = ''
 
-    def error_message(self, message):
-        print(message)
-        self.errors.append(message)
-
-    def warning_message(self, message):
-        print(message)
-        self.warnings.append(message)
-
-    def close(self):
+    def tearDown(self):
+        """
+        Runs after each test
+        """
         # delete temp files
-        if os.path.isdir(self.download_dir):
-            shutil.rmtree(self.download_dir, ignore_errors=True)
-        if os.path.isdir(self.files_dir):
-            shutil.rmtree(self.files_dir, ignore_errors=True)
+        if os.path.isdir(self.out_dir):
+            shutil.rmtree(self.out_dir, ignore_errors=True)
 
-    def run(self, internal_file=None): # optionally use internal file (useful for testing)
-        if not internal_file:
-            # download the archive
-            file_to_download = self.source_url
-            filename = self.source_url.rpartition('/')[2]
-            downloaded_file = os.path.join(self.download_dir, filename)
-            self.log_message('Downloading {0}...'.format(file_to_download))
-            if not os.path.isfile(downloaded_file):
-                try:
-                    download_file(file_to_download, downloaded_file)
-                finally:
-                    if not os.path.isfile(downloaded_file):
-                        raise Exception("Failed to download {0}".format(file_to_download))
-                    else:
-                        self.log_message('Download successful.')
-        else:
-            downloaded_file = internal_file
+    @classmethod
+    def setUpClass(cls):
+        """
+        Called before tests in this class are run
+        """
+        pass
 
-        # unzip the archive
-        self.log_message('Unzipping {0}...'.format(downloaded_file))
-        unzip(downloaded_file, self.files_dir)
-        self.log_message('Unzip successful.')
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Called after tests in this class are run
+        """
+        pass
 
-        # create output directory
-        make_dir(self.output_dir)
+    def test_close(self):
+        """
+        This tests that the temp directories are deleted when the class is closed
+        """
 
-        # read the markdown files and output html files
-        self.log_message('Processing the OBS markdown files')
+        with closing(TransformOBS('', '', True)) as tx:
+            download_dir = tx.download_dir
+            files_dir = tx.files_dir
 
-        files = sorted(glob(os.path.join(self.files_dir, '*')))
+            # verify the directories are present
+            self.assertTrue(os.path.isdir(download_dir))
+            self.assertTrue(os.path.isdir(files_dir))
 
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        with open(os.path.join(current_dir, 'obs-template.html')) as template_file:
-            html_template = string.Template(template_file.read())
+        # now they should have been deleted
+        self.assertFalse(os.path.isdir(download_dir))
+        self.assertFalse(os.path.isdir(files_dir))
 
-        complete_html = ''
-        for filename in files:
-            if filename.endswith('.md'):
-                # read the markdown file
-                with codecs.open(filename, 'r', 'utf-8-sig') as md_file:
-                    md = md_file.read()
-                html = markdown.markdown(md)
-                complete_html += html
-                html = html_template.safe_substitute(content=html)
-                base_name = os.path.splitext(os.path.basename(filename))[0]
-                html_filename = base_name + ".html"
-                output_file = os.path.join(self.output_dir, html_filename)
-                write_file(output_file, html)
-                self.log_message('Converted {0} to {1}.'.format(os.path.basename(filename), os.path.basename(html_filename)))
+    def test_run(self):
+        """
+        Runs the converter and verifies the output
+        """
 
-                # Do the OBS inspection
-                inspector = OBSInspection(output_file) # this now operates on a single file instead of folder
-                try:
-                    inspector.run()
-                except Exception as e:
-                    self.warning_message('Chapter {0}: failed to run OBS inspector: {1}'.format(base_name, e.message))
+        # print ("door43_tools.__version__={0}", door43_tools.__version__)
 
-                for warning in inspector.warnings:
-                    self.warning_message(warning)
-                for error in inspector.errors:
-                    self.error_message(error)
-        else:
-            # copy files that are not md files
-            try:
-                output_file = os.path.join(self.output_dir, filename[len(self.files_dir)+1:])
-                if not os.path.exists(output_file):
-                    if not os.path.exists(os.path.dirname(output_file)):
-                        os.makedirs(os.path.dirname(output_file))
-                    copyfile(filename, output_file)
-            except Exception:
-                pass
+        # test with the English OBS
+        file_name = 'aab_obs_text_obs_missing_fragment_01_01.zip'
+        repo_name = 'aab_obs_text_obs'
+        expected_warnings = 1
+        expected_errors= 0
+        zip_filepath = self.preprocessOBS(repo_name, file_name)
 
-        complete_html = html_template.safe_substitute(content=complete_html)
-        write_file(os.path.join(self.output_dir, 'all.html'), complete_html)
+        self.out_dir = tempfile.mkdtemp(prefix='txOBS_Test_')
+        with closing(TransformOBS(None, self.out_dir, True)) as tx:
+            tx.run(zip_filepath)
 
-        self.log_message('Made one HTML of all stories in all.html.')
-        self.log_message('Finished processing Markdown files.')
+        # verify the output
+        # 07 JAN 2017, NB: currently just one html file is being output, all.html
+        files_to_verify = []
+        for i in range(1, 51):
+            files_to_verify.append(str(i).zfill(2) + '.html')
 
+        for file_to_verify in files_to_verify:
+            file_name = os.path.join(self.out_dir, file_to_verify)
+            self.assertTrue(os.path.isfile(file_name), 'OBS HTML file not found: {0}'.format(file_name))
+
+        file_name = os.path.join(self.out_dir, 'all.html')
+        self.assertTrue(os.path.isfile(file_name), 'OBS HTML file not found: {0}'.format(file_name))
+        self.assertEqual(len(tx.warnings), expected_warnings)
+        self.assertEqual(len(tx.errors), expected_errors)
+
+
+    def preprocessOBS(self, repo_name, file_name):
+        file_path = os.path.join(self.resources_dir, file_name)
+
+        # 1) unzip the repo files
+        temp_dir = tempfile.mkdtemp(prefix='repo_')
+        unzip(file_path, temp_dir)
+        repo_dir = os.path.join(temp_dir, repo_name)
+        if not os.path.isdir(repo_dir):
+            repo_dir = file_path
+
+        # 2) Get the manifest file or make one if it doesn't exist based on meta.json, repo_name and file extensions
+        manifest_path = os.path.join(repo_dir, 'manifest.json')
+        if not os.path.isfile(manifest_path):
+            manifest_path = os.path.join(repo_dir, 'project.json')
+            if not os.path.isfile(manifest_path):
+                manifest_path = None
+        meta_path = os.path.join(repo_dir, 'meta.json')
+        meta = None
+        if os.path.isfile(meta_path):
+            meta = MetaData(meta_path)
+        manifest = Manifest(file_name=manifest_path, repo_name=repo_name, files_path=repo_dir, meta=meta)
+
+        # run OBS Preprocessor
+        output_dir = tempfile.mkdtemp(prefix='output_')
+        compiler = TsObsMarkdownPreprocessor(manifest, repo_dir, output_dir)
+        compiler.run()
+
+        # 3) Zip up the massaged files
+        # context.aws_request_id is a unique ID for this lambda call, so using it to not conflict with other requests
+        zip_filename = 'preprocessed.zip'
+        zip_filepath = os.path.join(file_path, zip_filename)
+        add_contents_to_zip(zip_filepath, output_dir)
+        if os.path.isfile(manifest_path) and not os.path.isfile(os.path.join(output_dir, 'manifest.json')):
+            add_file_to_zip(zip_filepath, manifest_path, 'manifest.json')
+
+        # delete temp files
+        shutil.rmtree(temp_dir, ignore_errors=True)
+
+        return zip_filepath
+
+
+
+if __name__ == '__main__':
+    unittest.main()
